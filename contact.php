e4654f82ee1df58973456638b938e9a2a35cdcf6
Mark <markphillip1800@gmail.com>
2023-06-11 14:40:49 +0200

    - Added an FAQ section in contact.php

--- contact.php
+++ contact.php
@@ -197,12 +197,165 @@
 
     </div>
 
+    <div class="container mt-5 scrollFade">
+        <h2 class="border-bottom pb-3 border-1 border-primary">FAQ</h2>
+        <div class="mt-3">
+            <div class="row">
+
+                <div class="col-12">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapseExample">
+                        Hvilke typer biler sælger I?
+                    </a>
+                    <div class="collapse" id="collapse-1">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Vi tilbyder et bredt udvalg af biler, herunder Peugeot, Fiat, Fiat Professional, Jeep, Mitsubishi og Hongqi i vores Slagelse afdeling og Peugeot, Citroën og Opel i vores Ringsted Afdeling.
+                                    Vi har forskellige modeller for at imødekomme forskellige præferencer og budgetter.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false" aria-controls="collapseExample">
+                        Er jeres køretøjer nye eller brugte?
+                    </a>
+                    <div class="collapse" id="collapse-2">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Vi har både nye og brugte køretøjer til salg. Vores nye biler kommer direkte fra producenterne,
+                                    mens vores brugte biler gennemgår en grundig inspektionsproces for at sikre deres kvalitet og pålidelighed.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapseExample">
+                        Hvad er prisområdet for jeres køretøjer?
+                    </a>
+                    <div class="collapse" id="collapse-3">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Vores køretøjer er tilgængelige i forskellige prisklasser for at imødekomme forskellige budgetter.
+                                    Prisområdet afhænger af faktorer som mærke, model, årgang, kilometertal og overordnet stand for køretøjet.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-4" aria-expanded="false" aria-controls="collapseExample">
+                        Tilbyder I finansieringsmuligheder?
+                    </a>
+                    <div class="collapse" id="collapse-4">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Ja, vi tilbyder finansieringsmuligheder for at gøre bilkøbet mere tilgængeligt.
+                                    Vi sørger altid for at tilbyde konkurrencedygtige rentesatser og fleksible betingelser,
+                                    der passer til vores kunders behov.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-5" aria-expanded="false" aria-controls="collapseExample">
+                        Hvad er processen for at ansøge om finansiering?
+                    </a>
+                    <div class="collapse" id="collapse-5">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Finansieringsprocessen begynder ved at udfylde en kreditansøgning.
+                                    Vi har brug for nogle grundlæggende oplysninger, herunder dine beskæftigelsesdetaljer,
+                                    indkomst og personlig identifikation. Når vi modtager din ansøgning,
+                                    vil vores salgsteam gennemgå den og arbejde sammen med dig for at finde de bedste finansieringsmuligheder, der er tilgængelige.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-6" aria-expanded="false" aria-controls="collapseExample">
+                        Hvordan kan jeg aftale en prøvetur?
+                    </a>
+                    <div class="collapse" id="collapse-6">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Du kan nemt aftale en prøvetur ved at kontakte vores forhandler via telefon, e-mail eller gennem vores hjemmeside.
+                                    Vores salgsteam vil hjælpe dig med at vælge en bekvem dato og tidspunkt for din prøvetur.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-7" aria-expanded="false" aria-controls="collapseExample">
+                        Hvilke garantimuligheder er der tilgængelige?
+                    </a>
+                    <div class="collapse" id="collapse-7">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Vi tilbyder forskellige garantimuligheder, herunder producentgarantier for nye køretøjer og udvidede garantier for brugte biler.
+                                    Disse garantier dækker specifikke komponenter og reparationer og giver dig ekstra tryghed.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-8" aria-expanded="false" aria-controls="collapseExample">
+                        Bliver jeres køretøjer inspiceret og certificeret?
+                    </a>
+                    <div class="collapse" id="collapse-8">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Ja, vores brugte køretøjer gennemgår en omfattende inspektionsproces udført af vores uddannede teknikere.
+                                    Vi undersøger omhyggeligt hvert køretøj for at sikre, at det lever op til vores kvalitetsstandarder og er sikkert for vores kunder at køre i.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 mt-2">
+                    <a class="btn btn-primary rounded-0 p-3 w-100 text-start fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-9" aria-expanded="false" aria-controls="collapseExample">
+                        Kan jeg købe en udvidet garanti?
+                    </a>
+                    <div class="collapse" id="collapse-9">
+                        <div class="card card-body rounded-0">
+                            <ul>
+                                <li>
+                                    Ja, vi tilbyder udvidede garantipakker for ekstra dækning udover producentens garanti.
+                                    Disse udvidede garantier giver ekstra beskyttelse mod uventede reparationer og kan tilpasses til dine specifikke behov.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Lokation -->
     <div class="scrollFade container mt-5 mb-5">
 
-        <h2 class="text-center text-lg-start">
-            <span class="d-inline-block fade-in-left _1-fade pb-3 pb-lg-0">FIND OS</span>
-        </h2>
+        <h2 class="border-bottom pb-3 border-1 border-primary">FIND OS</h2>
 
         <div class="row mt-3">
             <a class="map-image-container" href="https://www.google.com/maps?ll=55.414819,11.400031&z=16&t=m&hl=en&gl=DK&mapclient=embed&cid=4121818932834173153" target="_blank">
